be3af474fee9ba329e8d79bea638ea241edd893d
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-14 08:45:44 +0500

    Learning Numbers And Method In Js

---
+++ 01-Basics-Js/02-numbers.js
@@ -0,0 +1,14 @@
+// +++++++++++++++++++++ Numbers In Js ++++++++++++++++++++++++++
+
+let myNumber = 100 ;
+console.log(myNumber);
+
+let onlyNumber = new Number(100);
+console.log(myNumber);
+
+let numMethods = 400.00 ;
+console.log(numMethods.toExponential());
+console.log(numMethods.toFixed(3));
+console.log(numMethods.toLocaleString('ur-PK'));
+console.log(numMethods.toPrecision(7));
+console.log(numMethods.toString());
--- js-practice-fld/practice.js
+++ js-practice-fld/practice.js
@@ -10,3 +10,17 @@
 // console.log(myName.padStart(10 , "aska"));
 
 
+// +++++++++++++++++  Numbers +++++++++++++++++++++++++
+
+// let myNumber = 100 ;
+// console.log(myNumber);
+
+// let onlyNumber = new Number(100);
+// console.log(myNumber);
+
+// let numMethods = 40000 ;
+// console.log(numMethods.toExponential());
+// console.log(numMethods.toFixed(3));
+// console.log(numMethods.toLocaleString('ur-PK'));
+// console.log(numMethods.toPrecision(1));
+// console.log(numMethods.toString());
