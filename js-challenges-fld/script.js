b9552dd0d9cef1c589bbb808f92beebf21337921
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-14 12:38:19 +0500

    Js Mini Challenge Solve

--- js-challenges-fld/script.js
+++ js-challenges-fld/script.js
@@ -47,7 +47,7 @@
 // Challenge #4
 
 // let createCounter = (init) => {
-  
+
 // let currVal = init;
 
 //   return {
@@ -61,7 +61,7 @@
 
 //     reset: () => {
 
-//       currVal = init ;  
+//       currVal = init ;
 //       return currVal;
 //     },
 //   };
@@ -69,17 +69,16 @@
 
 // Challenge # 5
 
-
 // var map = (arr , fn) => {
 
 // let emptyArray = [ ] ;
 
 // for(let i = 0 ; i < arr.length ; i++){
 
-// let result = fn(arr[i] , i)    
+// let result = fn(arr[i] , i)
 // emptyArray.push(result)
 
-// }    
+// }
 
 // return emptyArray ;
 
@@ -97,3 +96,43 @@
 
 // console.log(`Student Name Is ${nameValue} Its Age ${age} and after 5 years ${afterAge}`);
 
+// Challenge no 7 ;
+
+// let item1Price = " ₹ 499.99 ";
+// let item2Price = "1200.50USD";
+// let couponCode = "SAVE20";
+// let userDiscountInput = "20"; 
+// let shippingFee = null;
+// let taxRate = undefined;
+
+// let item1 = item1Price.replace("₹", "").trim();
+// let cnvrtItm1 = new Number(item1);
+// console.log(cnvrtItm1);
+
+// let item2 = item2Price.replace("USD", "").trim();
+// let cnvrtItm2 = new Number(item2);
+// console.log(cnvrtItm2);
+
+// let subTotal = (cnvrtItm1 += cnvrtItm2);
+// console.log(subTotal);
+
+// let userDiscount = new Number(userDiscountInput);
+// console.log(userDiscount);
+
+// if (shippingFee === null) {
+//   console.log("Dilvery Free");
+// }
+
+// let finalPrice;
+
+// if (taxRate === undefined) {
+//   finalPrice = Math.max(0, subTotal - userDiscount).toFixed(2);
+// }
+
+// console.log(finalPrice);
+
+// if (finalPrice >= 1500) {
+//   console.log("Eligible for Premium Cashback");
+// } else {
+//   console.log("Standard Checkout");
+// }
