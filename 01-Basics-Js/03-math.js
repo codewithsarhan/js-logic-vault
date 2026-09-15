34b6f6840424467a08ae17d91f78654e26e8a96c
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-15 09:05:30 +0500

    Learn Math In Js

---
+++ 01-Basics-Js/03-math.js
@@ -0,0 +1,14 @@
+// ++++++++++++++++++++++++ Math ++++++++++++++++++++++++
+
+let min  = 10 ;
+let max = 30 ;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+let num = -110.20191;
+
+console.log(Math.abs(num));
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.round(num));
+console.log(Math.random());
