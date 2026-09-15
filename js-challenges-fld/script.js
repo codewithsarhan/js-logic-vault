000ea3a66de3a05d8ae2e7010d71d002d0d9132d
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-15 09:33:09 +0500

    Js Mini Challenge

--- js-challenges-fld/script.js
+++ js-challenges-fld/script.js
@@ -1,138 +1,54 @@
-// Challenge #1
+let customerName = prompt("Enter Your Name");
+let productPrice = prompt("Enter Your Price");
+let productQuanity = prompt("Enter Your Product Quanity");
+let deliveryCharges = 200;
+let discountCoupon = false;
+let customerType = "Regular";
+let subTotal = productPrice * productQuanity;
+let finalAmount = 0;
+let checkoutStatus = "Standard Checkout";
 
-// let createHelloWorld = () => {
+if (productPrice >= 6000) {
+  discountCoupon = true;
+  customerType = "Premium";
+} else {
+  discountCoupon = false;
+  customerType = "Regular";
+}
 
-//      return () => {
-//         return "Hello World";
-//     };
+if (customerType === "Premium" && productPrice >= 6000) {
+  finalAmount = subTotal - subTotal * (20 / 100);
+} else if (customerType === "Premium" || productPrice >= 6000) {
+  finalAmount = subTotal - subTotal * (10 / 100);
+} else if (discountCoupon === true) {
+  finalAmount = subTotal - subTotal * (5 / 100);
+} else {
+  alert("Buy First");
+}
 
-// }
-// const f = createHelloWorld();
-// console.log(f());
+if (finalAmount >= 6000) {
+  console.log("Delivery Free");
+} else {
+  finalAmount + deliveryCharges;
+}
 
-// Challenge #2
+if (finalAmount >= 10000) {
+  checkoutStatus = "Vip Checkout";
+} else if (finalAmount >= 7000) {
+  checkoutStatus = "Premium Checkout";
+} else if (finalAmount >= 5000) {
+  checkoutStatus = "Priority Checkout";
+} else {
+  checkoutStatus = "Standard Checkout";
+}
 
-// let createCounter =  (n) => {
+console.log(`Customer Name : ${customerName}`);
+console.log(`Customer Type : ${customerType}`);
+console.log(`Subtotal Price : ${subTotal}`);
+console.log(`Discount Coupon : ${discountCoupon}`);
+console.log(`Delivery Charges : ${deliveryCharges}`);
+console.log(`Final Amount : ${finalAmount}`);
+console.log(`Checkout Status : ${checkoutStatus}`);
 
-//     return () => {
 
-//         return n++
 
-//     }
-
-// }
-
-// Challenge #3
-
-// let expect = (val) => {
-//   return {
-//     toBe: (val2) => {
-//       if (val === val2) {
-//         return true;
-//       } else {
-//         throw new Error("Not Equal");
-//       }
-//     },
-
-//     notToBe: (val2) => {
-//       if (val !== val2) {
-//         return true;
-//       } else {
-//         throw new Error("Equal");
-//       }
-//     },
-//   };
-// };
-
-// Challenge #4
-
-// let createCounter = (init) => {
-
-// let currVal = init;
-
-//   return {
-//     increment: () => {
-//       return ++currVal;
-//     },
-
-//     decrement: () => {
-//       return --currVal;
-//     },
-
-//     reset: () => {
-
-//       currVal = init ;
-//       return currVal;
-//     },
-//   };
-// };
-
-// Challenge # 5
-
-// var map = (arr , fn) => {
-
-// let emptyArray = [ ] ;
-
-// for(let i = 0 ; i < arr.length ; i++){
-
-// let result = fn(arr[i] , i)
-// emptyArray.push(result)
-
-// }
-
-// return emptyArray ;
-
-// }
-
-// Challenge no 6 ;
-
-// let name = "  ali hassan "
-// let age = 25 ;
-// let isStudent = true ;
-
-// let nameValue = name.trim().toUpperCase();
-
-// let afterAge = age + 5 ;
-
-// console.log(`Student Name Is ${nameValue} Its Age ${age} and after 5 years ${afterAge}`);
-
-// Challenge no 7 ;
-
-// let item1Price = " ₹ 499.99 ";
-// let item2Price = "1200.50USD";
-// let couponCode = "SAVE20";
-// let userDiscountInput = "20"; 
-// let shippingFee = null;
-// let taxRate = undefined;
-
-// let item1 = item1Price.replace("₹", "").trim();
-// let cnvrtItm1 = new Number(item1);
-// console.log(cnvrtItm1);
-
-// let item2 = item2Price.replace("USD", "").trim();
-// let cnvrtItm2 = new Number(item2);
-// console.log(cnvrtItm2);
-
-// let subTotal = (cnvrtItm1 += cnvrtItm2);
-// console.log(subTotal);
-
-// let userDiscount = new Number(userDiscountInput);
-// console.log(userDiscount);
-
-// if (shippingFee === null) {
-//   console.log("Dilvery Free");
-// }
-
-// let finalPrice;
-
-// if (taxRate === undefined) {
-//   finalPrice = Math.max(0, subTotal - userDiscount).toFixed(2);
-// }
-
-// console.log(finalPrice);
-
-// if (finalPrice >= 1500) {
-//   console.log("Eligible for Premium Cashback");
-// } else {
-//   console.log("Standard Checkout");
-// }
