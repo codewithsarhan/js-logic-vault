23b98ee03445eab1ed257677bc1f07e0648b21a1
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-16 08:43:12 +0500

    Js MIni Challenge

--- js-challenges-fld/script.js
+++ js-challenges-fld/script.js
@@ -1,1 +1,20 @@
+// Mini Challenge Js ;
 
+let filter = (arr, fn) => {
+let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+
+
+if (fn(arr[i] , i)) {
+    
+result.push(arr[i]);
+
+}
+
+}
+
+
+return result ;
+};
