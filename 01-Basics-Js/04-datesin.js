cdcb084ed0fc1e496512bb0c23b36c5349cd0f1e
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-16 08:21:05 +0500

    Date In Js

---
+++ 01-Basics-Js/04-datesin.js
@@ -0,0 +1,7 @@
+// Date In Js 
+
+let myDate = new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getFullYear());
