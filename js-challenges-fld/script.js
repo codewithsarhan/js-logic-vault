afc932196b5edbc8321233143fe7e624fa8a2348
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-17 09:51:14 +0500

    Mini Challenge Js strings,number,dates,math,array

--- js-challenges-fld/script.js
+++ js-challenges-fld/script.js
@@ -1,13 +1,38 @@
-var reduce = (nums, fn, init) =>{
+// Smart Event Pass & Booking Manager ;
+
+let userNamePrompt = prompt("Enter Your Name");
+let promoCode = prompt("Enter Promo Code") ;
+
+let userName = userNamePrompt.toLowerCase().trim();
+console.log(userName);
+
+if (promoCode === "coding2026") {
   
-  let result = init ;
+console.log(`Promo Code Match : ${promoCode}`);
 
-  for(let i = 0 ; i < nums.length ; i++){
+}else {
 
-   result = (fn( result,nums[i] ));
+console.log(`Incorrect Promo Code`);
 
-  }
+}
 
-  return result ;
+let ticketPrice = 1500 ;
+let luckyDraw = Math.floor(Math.random() * (5 - 25 + 1) + 5);
+let ticktFnlPrc = ticketPrice - (ticketPrice * (luckyDraw / 100));
+console.log(ticktFnlPrc);
 
-};
+let startDate = new Date('2026-09-17');
+let futureDate = new Date('2026-10-1');
+
+let diffTim = futureDate - startDate ;
+let diffDays = diffTim / (1000 * 60 * 60 * 24 );
+
+console.log(`${diffDays.toFixed(0)} Days Later`);
+
+let allBooking = [
+
+  diffDays , ticktFnlPrc , promoCode , userName ,
+
+];
+
+console.log(allBooking);
