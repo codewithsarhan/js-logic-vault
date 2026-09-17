40a17e2a93495f429faa1870637d1d13ccbc8b49
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-17 12:46:56 +0500

    js mini challenge complete

--- js-challenges-fld/script.js
+++ js-challenges-fld/script.js
@@ -1,38 +1,39 @@
 // Smart Event Pass & Booking Manager ;
 
 let userNamePrompt = prompt("Enter Your Name");
-let promoCode = prompt("Enter Promo Code") ;
+let promoCode = prompt("Enter Promo Code");
 
 let userName = userNamePrompt.toLowerCase().trim();
 console.log(userName);
 
 if (promoCode === "coding2026") {
-  
-console.log(`Promo Code Match : ${promoCode}`);
-
-}else {
-
-console.log(`Incorrect Promo Code`);
-
+  console.log(`Promo Code Match : ${promoCode}`);
+} else {
+  console.log(`Incorrect Promo Code`);
 }
 
-let ticketPrice = 1500 ;
-let luckyDraw = Math.floor(Math.random() * (5 - 25 + 1) + 5);
-let ticktFnlPrc = ticketPrice - (ticketPrice * (luckyDraw / 100));
+let ticketPrice = 1500;
+let luckyDraw = Math.floor(Math.random() * (25 - 5 + 1) + 5);
+let ticktFnlPrc = ticketPrice - ticketPrice * (luckyDraw / 100);
 console.log(ticktFnlPrc);
 
-let startDate = new Date('2026-09-17');
-let futureDate = new Date('2026-10-1');
+let startDate = new Date("2026-09-17");
+let futureDate = new Date("2026-10-1");
 
-let diffTim = futureDate - startDate ;
-let diffDays = diffTim / (1000 * 60 * 60 * 24 );
+let diffTim = futureDate - startDate;
+let diffDays = diffTim / (1000 * 60 * 60 * 24);
 
 console.log(`${diffDays.toFixed(0)} Days Later`);
 
-let allBooking = [
+let allBooking = [];
 
-  diffDays , ticktFnlPrc , promoCode , userName ,
+let bookingObject = {
+  userName,
+  diffDays,
+  promoCode,
+  ticktFnlPrc,
+};
 
-];
-
-console.log(allBooking);
+let objectPush = allBooking.push(bookingObject);
+console.log(objectPush);
+console.log(allBooking[0]);
