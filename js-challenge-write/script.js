4347f97cb0cd779b2b14eb569d688a7db92916d9
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-17 13:09:50 +0500

    js challenges

--- js-challenge-write/script.js
+++ js-challenge-write/script.js
@@ -1,1 +1,40 @@
+// Smart Event Pass & Booking Manager ;
+
+let userNamePrompt = prompt("Enter Your Name");
+let promoCode = prompt("Enter Promo Code");
+
+let userName = userNamePrompt.toLowerCase().trim();
+console.log(userName);
+
+if (promoCode === "coding2026") {
+  console.log(`Promo Code Match : ${promoCode}`);
+} else {
+  console.log(`Incorrect Promo Code`);
+}
+
+let ticketPrice = 1500;
+let luckyDraw = Math.floor(Math.random() * (25 - 5 + 1) + 5);
+let ticktFnlPrc = ticketPrice - ticketPrice * (luckyDraw / 100);
+console.log(ticktFnlPrc);
+
+let startDate = new Date("2026-09-17");
+let futureDate = new Date("2026-10-1");
+
+let diffTim = futureDate - startDate;
+let diffDays = diffTim / (1000 * 60 * 60 * 24);
+
+console.log(`${diffDays.toFixed(0)} Days Later`);
+
+let allBooking = [];
+
+let bookingObject = {
+  userName,
+  diffDays,
+  promoCode,
+  ticktFnlPrc,
+};
+
+let objectPush = allBooking.push(bookingObject);
+console.log(objectPush);
+console.log(allBooking[0]);
 
