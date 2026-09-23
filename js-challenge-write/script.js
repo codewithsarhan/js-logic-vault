267441abcd06f8cfb48874d50973ad68ad96750b
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-23 11:42:20 +0500

    Js Challenge Write

--- js-challenge-write/script.js
+++ js-challenge-write/script.js
@@ -1,4 +1,4 @@
-//  ++++++++++++++++++++ Smart Student Report Card ++++++++++++++++++++
+// Smart Student Report Card :===
 
 let student = {
   name: "Sarhan",
@@ -20,8 +20,10 @@ let todayDate = new Date();
 let dateString = todayDate.toDateString();
 
 function generateReport() {
-  return `Student Name: ${student.name} | Max MArks : ${maxScore} | Avg : ${averageMarks} Today Date : ${dateString}`;
+  return `Student Name: ${student.name} | 
+  Max MArks : ${maxScore} | 
+  Avg : ${averageMarks} | 
+  Today Date : ${dateString}`;
 }
 
 console.log(generateReport());
-
