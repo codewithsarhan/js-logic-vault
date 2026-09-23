2b2517208ccb4290fbb10fe74063f67053787a42
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-23 08:26:43 +0500

    today i will learn function in js

---
+++ 01-Basics-Js/09-function.js
@@ -0,0 +1,40 @@
+// Today I Will Learn Function In JavaScript :====================
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+
+// You Log Many Time But Your Time Waste This methhod use function to your work is easy and clear
+
+// Function Made By Function Kewords And is use :===
+
+// function myNumbers (){
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+
+// }
+
+// myNumbers()
+
+
+// Parameters And Arrguments in funcion :====
+
+
+// function myFunction (num1 , num2){
+
+//     return num1 + num2
+
+// }
+
+// console.log(myFunction(2,3))
+
