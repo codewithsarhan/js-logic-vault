a653d32886472c49090fba0af9cecb0d061f57d4
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-24 09:20:36 +0500

    Student Data Handler

--- js-challenges-fld/script.js
+++ js-challenges-fld/script.js
@@ -1,26 +1,47 @@
-//  ++++++++++++++++++++ Smart Student Report Card ++++++++++++++++++++
+// "Student Data Handler"
 
-let student = {
-  name: "Sarhan",
-  age: 16,
-  subject: ["Math", "English", "Science"],
-};
+function calculateMarks(num1, num2, num3) {
 
-let marks = [85, 90, 78];
+  return console.log(
+    `Total Marks Is :==${num1 + num2 + num3}`
+  );
 
-let maxScore = Math.max(...marks);
-console.log(maxScore);
 
-let averageMarks = Math.round(
-  marks.reduce((sum, num) => sum + num, 0) / marks.length,
-);
-console.log(averageMarks);
-
-let todayDate = new Date();
-let dateString = todayDate.toDateString();
-
-function generateReport() {
-  return `Student Name: ${student.name} | Max MArks : ${maxScore} | Avg : ${averageMarks} Today Date : ${dateString}`;
 }
 
-console.log(generateReport());
+calculateMarks(200, 300, 400)
+
+function getAllScores(...scores) {
+
+  return console.log(scores)
+
+}
+
+getAllScores(100, 200, 600, 600, 700, 700)
+
+let student = {
+  studentName: "Sarhan",
+  rollNo: 101,
+  city: "Karachi"
+}
+
+function showStudentInfo(stundentInfo) {
+
+  return console.log(
+    `Student Name is ${stundentInfo.studentName}
+and student roll no is ${stundentInfo.rollNo}
+and student city is ${stundentInfo.city}`);
+
+}
+
+showStudentInfo(student);
+
+let subjectMarks = [100, 20, 40, 70, 10];
+
+function marksChecker(marks) {
+
+  return console.log(marks[3]);
+
+}
+
+marksChecker(subjectMarks);
