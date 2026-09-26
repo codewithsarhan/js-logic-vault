08ea74f077ae10c66d01a2c563518820d70a6d76
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-26 09:17:47 +0500

    Learn Block Scope And Global Scope In Js

---
+++ 01-Basics-Js/10-scope.js
@@ -0,0 +1,26 @@
+// Today It Will Be Discuss About Scope :==
+// Basically in programming scope have 2 type of scope
+// Firstly Block Scope 
+// Second Global Scope
+// what is scope ?
+// This {} cruly brackets its block Scope
+// You write inside anything {} braces you not access
+// out side tha brackets but you make any thing its
+// Outside of brackets you also access in curly brackets
+// Got It 
+
+// let a = 10 ;
+// const b = 20  ;
+// var c = 30 ;
+
+if (true) {
+
+ let a = 30;
+ const b = 50 ;
+ var c = 100 ;
+
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
